97537df83521c6c1f5d04e0c2d90ff4125cf9160
Takeda Haruna <67426438+haru-programming@users.noreply.github.com>
2020-08-03 23:29:31 +0900

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 $(function () {
   $(".header").bgSwitcher({
     images: [
-      "../img/fv-bgi_01@2x.jpg",
-      "../img/fv-bgi_02@2x.jpg",
-      "../img/fv-bgi_03@2x.jpg",
+      "https://github.com/haru-programming/coding_practice--2/blob/master/img/fv-bgi_01@2x.jpg?raw=true",
+      "https://github.com/haru-programming/coding_practice--2/blob/master/img/fv-bgi_02@2x.jpg?raw=true",
+      "https://github.com/haru-programming/coding_practice--2/blob/master/img/fv-bgi_03@2x.jpg?raw=true",
     ],
     interval: 3000,
   });
