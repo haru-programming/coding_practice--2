7da1874cc8ab5b542bbe2a4d031991fe649f52ab
h136479442@gmail.com <h136479442@gmail.com>
2020-08-04 14:57:41 +0900

    error handling bg slider

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,13 @@
-$(function () {
-  $(".header").bgSwitcher({
-    images: [
-      "https://github.com/haru-programming/coding_practice--2/blob/master/img/fv-bgi_01@2x.jpg?raw=true",
-      "https://github.com/haru-programming/coding_practice--2/blob/master/img/fv-bgi_02@2x.jpg?raw=true",
-      "https://github.com/haru-programming/coding_practice--2/blob/master/img/fv-bgi_03@2x.jpg?raw=true",
-    ],
-    interval: 3000,
+$(document).on("ready", function () {
+  $(".bg").slick({
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 3000,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
   });
 });
